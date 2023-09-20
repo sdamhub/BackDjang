640dd6a2b45db9f5091e0f20bf5af7851f2ac752
sdamhub <solomondamilola42@gmail.com>
2023-09-20 10:06:51 +0100

    Created the login page, added it to path in url, authenticated the user during registration and redirected them after successful registration to the login page. In the views page, I created a function for the loin page just like I did for the register page. Basically for the purpose of authentication. On the login page, authenticated the user, after successful authentication, redirect them to the index/home page.

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -10,6 +10,10 @@ def index(request):
 
     return render(request, 'index.html', {'features' : features})
 
+
+
+
+
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -34,6 +38,21 @@ def register(request):
             return  redirect('register')
     else:
         return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'User not found')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
 
 def counter(request):
     texts = request.POST['texts']
