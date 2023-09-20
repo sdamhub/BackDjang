77ab709e678c53703c779043633aa5befe945368
sdamhub <solomondamilola42@gmail.com>
2023-09-20 15:23:26 +0100

    This setting is to integrate postgres as the new database manager for the project. The password required is the password for creating the postgres when setting up the postgres database

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql', #it was previously sqlite3
+        'NAME': 'myproject',   # previously - BASE_DIR / 'db.sqlite3',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost'
+
     }
 }
 
