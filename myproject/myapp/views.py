24deeca760cb24d273dd26f9b375eea9964c14bd
sdamhub <solomondamilola42@gmail.com>
2023-09-20 09:21:51 +0100

    with the successful connection of the register path in the urls.py page, a series of if statements are created in the views.py page to validate the input of the user during registration. At the register page, I used the special for loop to introduce the messages written via the if statement in the views.py page, and styled it via the style tag. These affects the data in the db.sqlite3 page, although I had made some modifications to it before this commit

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
@@ -7,6 +9,31 @@ def index(request):
     features = Feature.objects.all()
 
     return render(request, 'index.html', {'features' : features})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'User Name already in use')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save();
+                return redirect('login')
+
+        else:
+            messages.info(request, 'password not the same')
+            return  redirect('register')
+    else:
+        return render(request, 'register.html')
 
 def counter(request):
     texts = request.POST['texts']
